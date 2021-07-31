c716b850fb24c3c78af379ef68b76b0df9479481
GS-Moonlyn <gsmolive@gmail.com>
2021-07-31 18:49:24 -0300

    Função de corrida do Grilo

--- src/objeto2/Grilo.java
+++ src/objeto2/Grilo.java
@@ -3,6 +3,10 @@ package objeto2;
 public class Grilo extends Thread {
 	
 	private String nome;
+	private int min = 10;
+	private int max = 30;
+	private int posicaoAtual;
+	private int chegada = 500;
 	
 	//Construtor
 	public Grilo(String nome) {
@@ -11,12 +15,21 @@ public class Grilo extends Thread {
 	}
 
 	public void run() {
-		//Log de exibição da Thread 
+		//Exibe a Thread atual 
 		Thread t = Thread.currentThread();
 		System.out.println(t.getName());
 		
-		//Log de distância percorrida pelo Grilo
-		System.out.println(nome + " pulou " + "10cm");
+		//Log de distancia percorrida pelo Grilo
+		while(posicaoAtual < chegada) {
+			int pulo = (int)Math.floor(Math.random()*(max-min+1)+min);
+			posicaoAtual += pulo;
+			System.out.println(nome + " pulou " + pulo + "cm. " + nome + " percorreu " + posicaoAtual + "cm");
+		}
+		
+		//Exibe que o Grilo atingiu a linha de chegada
+		if(posicaoAtual >= chegada) {
+			System.out.println(nome + " atingiu a linha de chegada!");
+		}
 		
 	}
 }
--- src/objeto2/mainApp.java
+++ src/objeto2/mainApp.java
@@ -1,7 +1,7 @@
 package objeto2;
 
 public class mainApp {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			Thread t = Thread.currentThread();
